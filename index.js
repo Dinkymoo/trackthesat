7b08240b957f6cc1b90da741da894c002217592f
Dinkymoo <danielletulp@yahoo.com>
2025-01-23 20:39:26 +0100

    update app url

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 async function initMap() {
   // Request needed libraries.
   const { Map } = await google.maps.importLibrary('maps');
-  const { AdvancedMarkerElement } = await google.maps.importLibrary('marker');
-  const cowImg = document.createElement('img');
-  cowImg.src = './assets/image/crazyCow.png';
+  const { Marker } = await google.maps.importLibrary('marker');
 
   try {
-    const response = await fetch('http://api.open-notify.org/iss-now.json', { referrerPolicy: 'unsafe-url' });
+    const response = await fetch('https://cowsatnav.azurewebsites.net/api/cowSatNavFunc?code=O2rKrzg2URQil2XBB0c7arLz4x9eUySa1qbONSqK8z-8AzFuPvCEEg%3D%3D');
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -16,21 +14,22 @@ async function initMap() {
 
     const map = new Map(document.getElementById('map'), {
       center: { lat: latitude, lng: longitude },
-      zoom: 5,
+      zoom: 4,
       mapId: '911e7beff6d83398',
     });
     map.setCenter({ lat: latitude, lng: longitude });
 
-    const cowMarker = new AdvancedMarkerElement({
+    // const cowImg = document.createElement('img');
+    // cowImg.src = '/image/crazyCow.png';
+
+    const cowMarker = new Marker({
       map,
       position: { lat: latitude, lng: longitude },
-      content: cowImg,
-      title: 'A marker using a custom PNG Image',
+      //   content: cowImg,
+      //   title: 'A marker using a custom PNG Image',
     });
-    cowMarker.setMap(map);
   } catch (error) {
     console.error('Fetch error: ', error);
   }
 }
-
 initMap();
