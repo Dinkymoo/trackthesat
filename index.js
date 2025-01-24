0fee9f286dc91d899f30fa7e2dca23ad7b512ffa
Dinkymoo <danielletulp@yahoo.com>
2025-01-24 09:41:37 +0100

    update url and marker logic

--- index.js
+++ index.js
@@ -2,35 +2,29 @@ async function initMap() {
   // Request needed libraries.
   const { Map } = await google.maps.importLibrary('maps');
   const { Marker } = await google.maps.importLibrary('marker');
+  const url = 'https://cowsatnav.azurewebsites.net/api/cowSatNavFunc?code=O2rKrzg2URQil2XBB0c7arLz4x9eUySa1qbONSqK8z-8AzFuPvCEEg%3D%3D';
+  // const url = 'http://localhost:7071/api/cowSatNavFunc';
+  let response = await fetch(url);
+  const data = await response.json();
+  debugger;
+  const latitude = Number(data['iss_position'].latitude);
+  const longitude = Number(data['iss_position'].longitude);
 
-  try {
-    const response = await fetch('https://cowsatnav.azurewebsites.net/api/cowSatNavFunc?code=O2rKrzg2URQil2XBB0c7arLz4x9eUySa1qbONSqK8z-8AzFuPvCEEg%3D%3D');
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const data = await response.json();
-    const latitude = Number(data['iss_position'].latitude);
-    const longitude = Number(data['iss_position'].longitude);
+  const map = new Map(document.getElementById('map'), {
+    center: { lat: latitude, lng: longitude },
+    zoom: 4,
+    mapId: '911e7beff6d83398',
+  });
+  map.setCenter({ lat: latitude, lng: longitude });
 
-    const map = new Map(document.getElementById('map'), {
-      center: { lat: latitude, lng: longitude },
-      zoom: 4,
-      mapId: '911e7beff6d83398',
-    });
-    map.setCenter({ lat: latitude, lng: longitude });
+  // const cowImg = document.createElement('img');
+  // cowImg.src = '/image/crazyCow.png';
 
-    // const cowImg = document.createElement('img');
-    // cowImg.src = '/image/crazyCow.png';
-
-    const cowMarker = new Marker({
-      map,
-      position: { lat: latitude, lng: longitude },
-      //   content: cowImg,
-      //   title: 'A marker using a custom PNG Image',
-    });
-  } catch (error) {
-    console.error('Fetch error: ', error);
-  }
+  const cowMarker = new Marker({
+    map,
+    position: { lat: latitude, lng: longitude },
+    //   content: cowImg,
+    //   title: 'A marker using a custom PNG Image',
+  });
 }
-
 initMap();
