72bc07eebafde9c2661970ba1b1b092ccbfcc0ef
Dinkymoo <danielletulp@yahoo.com>
2025-01-24 15:26:00 +0100

    fix cow icon

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ async function initMap() {
   // Request needed libraries.
   const { Map } = await google.maps.importLibrary('maps');
   const { Marker } = await google.maps.importLibrary('marker');
+  // Fetch the data from the Azure Function.
   const url = 'https://cowsatnav.azurewebsites.net/api/cowSatNavFunc?code=O2rKrzg2URQil2XBB0c7arLz4x9eUySa1qbONSqK8z-8AzFuPvCEEg%3D%3D';
   // const url = 'http://localhost:7071/api/cowSatNavFunc';
   let response = await fetch(url);
   const data = await response.json();
-  debugger;
   const latitude = Number(data['iss_position'].latitude);
   const longitude = Number(data['iss_position'].longitude);
 
@@ -15,16 +15,20 @@ async function initMap() {
     zoom: 4,
     mapId: '911e7beff6d83398',
   });
+
   map.setCenter({ lat: latitude, lng: longitude });
 
-  // const cowImg = document.createElement('img');
-  // cowImg.src = '/image/crazyCow.png';
+  const icon = {
+    url: 'http://localhost:8080/public/image/cow.png', // url
+    scaledSize: new google.maps.Size(500, 500), // scaled size
+    origin: new google.maps.Point(0, 0), // origin
+    anchor: new google.maps.Point(0, 0), // anchor
+  };
 
-  const cowMarker = new Marker({
-    map,
+  new Marker({
+    map: map,
     position: { lat: latitude, lng: longitude },
-    //   content: cowImg,
-    //   title: 'A marker using a custom PNG Image',
+    icon: icon,
   });
 }
 initMap();
